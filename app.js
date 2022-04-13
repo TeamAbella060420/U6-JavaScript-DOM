dcec7515a416b1eee95a72061b848f6ad203cc2a
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-13 10:42:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ btnCreate.addEventListener('click', () => {
   const input = document.querySelector('.input-main');
   const list = document.querySelector('ul');    
 
+  input.value = '';                 
   list.insertAdjacentHTML(
       'afterbegin',
-      `<li>${input.value}</li>`
+      `<li>${input.value}</li>`     
   );  
-  input.value = '';
-});
+});                              // These syntax added the task that you type from the input box and listed it below.
 
 btnToggle.addEventListener('click', () => {
     const listContainer = document.querySelector('.list-container');
@@ -23,9 +23,9 @@ btnToggle.addEventListener('click', () => {
         btnToggle.textContent = 'Show List';
         listContainer.style.display = 'none';
     }
-});
+});                              // These syntax is for the hide list and show list click button.
 
 btnRemove.addEventListener( 'click', ( ) => {
     const lastItem = document.querySelector('li:last-child');
     lastItem.remove();
-});
+});                              // These syntaxt is for the red buuton at the bottom to remove the last task on the list.
